1feacd52c07f2cdbf0e508de05cba088e6201644
Arman Ashrafian <ashy63@gmail.com>
2017-03-26 07:01:41 +0000

    removed stuff

--- __init__.py
+++ __init__.py
@@ -1,33 +1,12 @@
+
 # Server
 
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
 import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///messages.sqlite3'
-db = SQLAlchemy(app)
-
-
-class Message(db.Model):
-    id = db.Column('messageID', db.Integer, primary_key=True)
-    mess = db.Column(db.Text(500))
-    date = db.Column(db.String(10))
-
-    # Constructor
-    def __init__(self, mess, date):
-        self.mess = mess
-        self.date = date
-
-    # Overwrite print
-    def __repr__(self):
-        return '%s -> %s' % self.date, self.mess
-
 @app.route('/')
 def index():
-    message = Message('HELLO WORLD', getToday())
-    db.session().add(message)
-    db.session().commit()
     return render_template('index.html')
 
 
