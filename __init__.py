715d3eb9d0f248b5ec41d5475f9d7336b88ea6fa
Arman Ashrafian <ashy63@gmail.com>
2017-03-31 11:19:03 -0700

    added example database

--- __init__.py
+++ __init__.py
@@ -1,14 +1,39 @@
 
 # Server
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Example(db.Model):
+    __tablename__ = "Example"
+    id = db.Column('id', db.Integer, primary_key=True)
+    data = db.Column('data', db.Unicode)
+
+    # Constructor
+    def __init__(self, text):
+        self.data = text
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/new', methods=["POST"])
+def new():
+    if(request.method == "POST"):
+        userText = request.form['text']
+        if(userText != None):
+            dbEntery = Example(userText)
+            db.session.add(dbEntery)
+            db.session.commit()
+
+    return render_template('index.html')
 
 # Get Today
 # returns formatted date as a string
@@ -22,4 +47,4 @@ def getToday():
 
 if __name__ == "__main__":
     # start server
-    app.run()
+    app.run(debug=True)
